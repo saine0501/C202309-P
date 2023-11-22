43f359b5ae9bc1a747254d026f0418e162d93bf0
saine0501 <saine0501@gmail.com>
2023-11-22 23:56:30 +0900

    update password

--- sources/mentoring.c
+++ sources/mentoring.c
@@ -4,8 +4,11 @@
 int choice = -1;
 char password[10];
 
+void getPassword();
+
 int main() {
 	int terminate = 0;
+	int try = 0;
 
 	scanf_s("%d", &choice);
 
@@ -18,7 +21,7 @@ int main() {
 			printf("잠시 후 오후 7시 30분에 멘토링이 진행될 예정입니다~! 교재와 함께 시간 맞춰 접속해주세요 :)\n");
 			printf("잠시 후 오후 8시 30분에 멘토링이 진행될 예정입니다~! 교재와 함께 시간 맞춰 접속해주세요 :)\n");
 			printf("잠시 후 오후 9시에 멘토링이 진행될 예정입니다~! 교재와 함께 시간 맞춰 접속해주세요 :)\n");
-                        
+
 			break;
 
 
@@ -28,14 +31,7 @@ int main() {
 			break;
 
 		case 3: // 3번 틀리면 프로그램 종료? 멘토 기능 하나 더 추가?
-			printf("비밀번호를 입력하세요.");
-			scanf_s("%s", password, (int)sizeof(password));
-			if (strcmp(password, "mentoring") == 0) {
-				printf("aa\n");
-			}
-			else {
-				printf("틀렸습니다. 다시 입력해주세요. 기회는 %d번 남았습니다. \n", 3); // 3번 이상 틀리면 프로그램 종료
-			}
+			getPassword();
 			break;
 
 		case 4:
@@ -50,4 +46,28 @@ int main() {
 	}
 
 	return 0;
+}
+
+void getPassword() {
+	int terminate = 0;
+	int try = 0;
+
+	while (1) {
+		printf("비밀번호를 입력하세요: ");
+		scanf_s("%s", password, (int)sizeof(password));
+
+		if (strcmp(password, "mentoring") == 0) {
+			printf("맞았습니다!\n");
+			return;
+		}
+		else {
+			try++;
+			printf("틀렸습니다. 다시 입력해주세요. 기회는 %d번 남았습니다.\n", 3 - try);
+
+			if (try >= 3) {
+				printf("프로그램을 종료합니다. 너무 많은 시도 실패.\n");
+				terminate = 1;
+			}
+		}
+	}
 }
